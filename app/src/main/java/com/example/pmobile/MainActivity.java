cf6f4c68c63bdef5411db699281d14d66f57a111
060_Muhammad irfan fajru ramadhan <irfanfajru025@gmail.com>
2021-11-03 01:31:50 +0700

    penambahan activity life cycle

--- app/src/main/java/com/example/pmobile/MainActivity.java
+++ app/src/main/java/com/example/pmobile/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.pmobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("onCreate");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        System.out.println("onStart");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        System.out.println("onRestart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        System.out.println("onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        System.out.println("onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        System.out.println("onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        System.out.println("onDestroy");
     }
 }
