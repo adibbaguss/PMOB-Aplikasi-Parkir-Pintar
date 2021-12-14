78a66ad6f2f668e7204567dfc94ec626cdb2cde7
adibbaguss <adibbagus42@gmail.com>
2021-12-14 21:10:07 +0700

    14 12 2021

---
+++ app/src/main/java/com/example/pmobile/InformasiParkirActivity.java
@@ -0,0 +1,14 @@
+package com.example.pmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformasiParkirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi_parkir);
+    }
+}
